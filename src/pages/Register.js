f932afc43410a399e7b02be0cbba084f3fd8b4ac
rohit <rohitchatla@gmail.com>
2021-12-30 21:05:10 +0530

    init

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -52,7 +52,7 @@ export default function Register() {
       <MHidden width="mdDown">
         <SectionStyle>
           <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-            Manage the job more effectively with Minimal
+            One stop DataScience DeepNeural Engine @ Covid Genesis
           </Typography>
           <img alt="register" src="/static/illustrations/illustration_register.png" />
         </SectionStyle>

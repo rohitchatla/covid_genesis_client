347fe3d335d36025e5070161f68487b0cf64f739
rohit <rohitchatla@gmail.com>
2021-12-31 08:55:12 +0530

    changes

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,12 +1,12 @@
 import Axios from 'axios';
 
-const baseURL = 'http://localhost:5000/api';
-const assestsURL = 'http://localhost:5000/assets/';
-const socketCon = 'http://localhost:5000';
+// const baseURL = 'http://localhost:5000/api';
+// const assestsURL = 'http://localhost:5000/assets/';
+// const socketCon = 'http://localhost:5000';
 
-// const baseURL = 'https://covidgenesis-server.herokuapp.com/api';
-// const assestsURL = 'https://covidgenesis-server.herokuapp.com/assets/';
-// const socketCon = 'https://covidgenesis-server.herokuapp.com';
+const baseURL = 'https://covidgenesis-server.herokuapp.com/api';
+const assestsURL = 'https://covidgenesis-server.herokuapp.com/assets/';
+const socketCon = 'https://covidgenesis-server.herokuapp.com';
 
 Axios.defaults.baseURL = baseURL;
 
